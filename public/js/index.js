f95c6267514f116f687cd2cdadf87f4e35ed2b1b
mimoncopitman <mimoncopitman@gmail.com>
2018-06-03 16:40:53 +0300

    - add jquery

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,17 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('New message', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
 });
 
+$('#message_form').on('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function () {
 
+    });
+});
