583e9fb14c6e5d8fc8b3994cebe755c25a61d1d7
mimoncopitman <mimoncopitman@gmail.com>
2018-06-03 15:57:37 +0300

    - add message test file and messages file for generateMessage function.

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,12 @@
 var socket = io();
 
 socket.on('connect', function () {
-    console.log('New user connected');
+    console.log('Conected to web');
 
-    socket.emit('createEmail', {
-        to: 'jen@gmail.com',
-        text: 'Hey jen. how are you'
-    });
-
-
-    socket.emit('createMessage', {
-        from: 'mike@examel.com',
-        text: 'hey hat is goung on'
-    });
+    // socket.emit('createMessage', {
+    //     from: 'mike@examel.com',
+    //     text: 'hey hat is goung on',
+    // });
 
 });
 
@@ -20,13 +14,8 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function (email) {
-    console.log('New email', email);
-});
-
 socket.on('newMessage', function (message) {
     console.log('New message', message);
 });
-
 
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,24 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newEmail', {
-        from: 'mike@examel.com',
-        text: 'hey hat is goung on',
-        createAt: 123
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app.'));
 
-    socket.emit('newMessage', {
-        from: 'mike@examel.com',
-        text: 'hey hat is goung on form server',
-        createAt: 123
-    });
-
-    socket.on('createEmail', (newEmail) => {
-        console.log('createEmail', newEmail);
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message) => {
-        console.log('message', message);
+        console.log('createMessage', message);
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
 
     socket.on('disconnect', () => {
